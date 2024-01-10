11f3c1fa33b669b3cfd2b0e937514b3c4d735df5
kastastin <kostya242421@gmail.com>
2024-01-10 16:26:19 +0200

    AuthLayout component

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,50 +1,11 @@
-import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
-
-import Logo from "../ui/Logo";
+import AuthLayout from "../ui/AuthLayout";
 import LoginForm from "../features/authentication/LoginForm";
 
-const ImageContainer = styled.div`
-  width: 15rem;
-  position: absolute;
-  top: 2rem;
-  cursor: pointer;
-
-  div img {
-    z-index: 1;
-  }
-
-  @media (max-width: 560px) {
-    left: 50%;
-    transform: translateX(-50%);
-  }
-`;
-
-const PageLayout = styled.main`
-  min-height: 100vh;
-  background: no-repeat 20% 50%/110% url("/login-bg.jpg");
-
-  display: flex;
-  justify-content: flex-end;
-
-  @media (max-width: 560px) {
-    justify-content: center;
-    background: #fff;
-  }
-`;
-
 function Login() {
-  const navigate = useNavigate();
-
   return (
-    <>
-      <ImageContainer onClick={() => navigate("/")}>
-        <Logo type="vertical" />
-      </ImageContainer>
-      <PageLayout>
-        <LoginForm />
-      </PageLayout>
-    </>
+    <AuthLayout>
+      <LoginForm />
+    </AuthLayout>
   );
 }
 
