e369e9c393f8d8dcd0fb8a7f3639c328e5a3ae07
KripakDebug <kastrula123@gmail.com>
2024-01-27 09:50:09 +0200

    add profile photo

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import './Profile.scss'
-function Profile() {
-    return (
-      <div className="wrapper">
-          <div className='profile'>
-            Profile
+import React from "react";
+import "./Profile.scss";
+import { Image } from "antd";
+function Profile({name, balance}) {
+  return (
+    <div className="wrapper">
+      <div className="profile">
+          <div className="profile__head">
+            <div className="user-info">
+              <Image
+                width={170}
+                style={{borderRadius: '100%'}}
+                src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+              />
+              <div className="user-info__name">
+                  {name || 'Masha Petrenko'}
+              </div>
+              <div className="user-info__balance">
+                  Вalance: {balance || '1,000'} AiCoin
+            </div>
+            </div>
           </div>
+          <div className="profile__info">
+            
+          </div>
+        </div>
       </div>
-    );
-};
+  );
+}
 
 export default Profile;
