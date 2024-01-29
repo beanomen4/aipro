09b99e71333b45d7a253c0a9be6f4509e32e9d1a
kastastin <kostya242421@gmail.com>
2024-01-29 17:44:13 +0200

    logout [btn in Test component] (#11)

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,21 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
-import { Spin } from "antd";
+import { Button, Spin } from "antd";
 
 import { getClients } from "../services/apiClients";
+import { useLogout } from "../features/authentication/useLogout";
 
 function Test() {
-  const { data: clients, isLoading } = useQuery({
+  const { logout, isLoading } = useLogout();
+
+  const { data: clients, isLoading: isLoadingClients } = useQuery({
     queryKey: ["clients"],
     queryFn: getClients,
   });
 
-  if (isLoading) return <Spin />;
+  if (isLoadingClients) return <Spin />;
 
   return (
     <>
       <div>id: {clients?.[0].id}</div>
       <div>username: {clients?.[0].username}</div>
       <div>aicoin: {clients?.[0].aicoin}</div>
+      <Button type="primary" disabled={isLoading} onClick={logout}>
+        logout
+      </Button>
     </>
   );
 }
