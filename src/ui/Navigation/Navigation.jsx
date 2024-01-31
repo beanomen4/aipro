1a47ab4473a4b5fc37432aca328a19a7c7d8ff2d
KripakDebug <kastrula123@gmail.com>
2024-01-31 16:46:07 +0200

    add burgerMeny

--- src/ui/Navigation/Navigation.jsx
+++ src/ui/Navigation/Navigation.jsx
@@ -1,26 +1,50 @@
 import { Link } from "react-router-dom";
-import { Avatar, ConfigProvider, Select, theme } from "antd";
+import { Avatar, ConfigProvider, Dropdown, Select, theme } from "antd";
 import { UserOutlined } from "@ant-design/icons";
-
+import logo from "./../../assets/logo-icon.svg";
 import "./Navigation.scss";
 
 function Navigation() {
+  const items = [
+    {
+      label: <Link to={'/'}>AiPro</Link>,
+      key: '0',
+    },
+    {
+      label: <Link to={'/'}>My account</Link>,
+      key: '1',
+    },
+    {
+      label: <Link to={'/'}>Applications</Link>,
+      key: '2',
+      children: [
+        {
+          key: '2-1',
+          label: 'All applications',
+        },
+        {
+          key: '2-2',
+          label: 'My applications',
+        },
+      ],
+    },
+    {
+      label: <Link to={'/'}>Exit</Link>,
+      key: '3',
+    },
+  ];
+
   return (
+    <ConfigProvider
+    theme={{
+      algorithm: theme.darkAlgorithm,
+    }}
+  >
     <nav className="nav">
       <Link className="logo">
-        {/*
-          Vercel doesn't display logo
-          'src/assets/logo-icon.svg' - doesn't work
-          '/src/assets/logo-icon.svg' - doesn't work
-         */}
-        AiPro <img src="src/assets/logo-icon.svg" alt="Logo AiPro" />
+        AiPro <img src={logo} alt="Logo AiPro" />
       </Link>
       <div className="user">
-        <ConfigProvider
-          theme={{
-            algorithm: theme.darkAlgorithm,
-          }}
-        >
           <Select
             defaultValue="Ua"
             className="change-lang"
@@ -35,13 +59,23 @@ function Navigation() {
               },
             ]}
           />
-        </ConfigProvider>
-        <Avatar
+            <Dropdown
+            overlayClassName="dropdown-menu"
+              menu={{
+                items,
+              }}
+              placement="bottomRight"
+              trigger={["click"]}
+            >
+               <Avatar
+          className="avatar"
           size="large"
           src={"https://i.mydramalist.com/66L5p_5c.jpg" || <UserOutlined />}
         />
+            </Dropdown>
       </div>
     </nav>
+    </ConfigProvider>
   );
 }
 
