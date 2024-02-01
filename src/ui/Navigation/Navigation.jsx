5647406294a3d6578ae05c14c7352ce1537de22b
KripakDebug <kastrula123@gmail.com>
2024-02-01 13:42:57 +0200

    add adaptive navigation

--- src/ui/Navigation/Navigation.jsx
+++ src/ui/Navigation/Navigation.jsx
@@ -1,55 +1,69 @@
 import { Link } from "react-router-dom";
-import { Avatar, ConfigProvider, Dropdown, Select, theme } from "antd";
-import { UserOutlined } from "@ant-design/icons";
+import { Avatar, ConfigProvider, Dropdown, Menu, Select, theme } from "antd";
+import { LeftOutlined, UserOutlined } from "@ant-design/icons";
 import { useUser } from "../../features/authentication/useUser";
 import { useLogout } from "../../features/authentication/useLogout";
 import logo from "./../../assets/logo-icon.svg";
 import "./Navigation.scss";
-
+import { useEffect, useState } from "react";
+import SubMenu from "antd/es/menu/SubMenu";
+import ButtonForIcon from "../ButtonForIcon";
 function Navigation() {
   const { logout } = useLogout();
   const { user } = useUser();
-  const { picture } = user.user_metadata;
-  const items = [
-    {
-      label: <Link to={'/'}>AiPro</Link>,
-      key: '0',
-    },
-    {
-      label: <Link to={'/'}>My account</Link>,
-      key: '1',
-    },
-    {
-      label: <Link to={'/'}>Applications</Link>,
-      key: '2',
-      children: [
-        {
-          key: '2-1',
-          label: 'All applications',
-        },
-        {
-          key: '2-2',
-          label: 'My applications',
-        },
-      ],
-    },
-    {
-      label: <Link onClick={logout}>Exit</Link>,
-      key: '3',
-    },
-  ];
+  const { picture, name } = user.user_metadata;
+  const [matches, setMatches] = useState(
+    window.matchMedia("(max-width: 640px)").matches
+  );
 
+  useEffect(() => {
+    window
+      .matchMedia("(max-width: 640px)")
+      .addEventListener("change", (e) => setMatches(e.matches));
+  }, []);
+  const menu = (
+    <Menu>
+      <Menu.Item key="0" className="close-menu">
+        <LeftOutlined /> Personal Area
+      </Menu.Item>
+      <div className="user-info">
+        <div className="user-photo">
+          <img width={170} src={picture} />
+        </div>
+        <div className="user-info-main">
+          <div className="user-info__name">{name}</div>
+          <div className="user-info__balance">Вalance: {"0"} AiCoin</div>
+        </div>
+      </div>
+      <Menu.Item key="1">AiPro</Menu.Item>
+      <Menu.Item key="2">My account</Menu.Item>
+      {matches ? (
+        <>
+          <Menu.Item key="3">All applications</Menu.Item>
+          <Menu.Item key="4">My applications</Menu.Item>
+        </>
+      ) : (
+        <SubMenu key="subMenu" title="Application">
+          <Menu.Item key="0">All applications</Menu.Item>
+          <Menu.Item key="1">My applications</Menu.Item>
+        </SubMenu>
+      )}
+      <Menu.Item onClick={logout} key="5">
+        Logout
+      </Menu.Item>
+    </Menu>
+  );
   return (
     <ConfigProvider
-    theme={{
-      algorithm: theme.darkAlgorithm,
-    }}
-  >
-    <nav className="nav">
-      <Link className="logo">
-        AiPro <img src={logo} alt="Logo AiPro" />
-      </Link>
-      <div className="user">
+      theme={{
+        algorithm: theme.darkAlgorithm,
+      }}
+    >
+      <nav className="nav">
+        <Link className="logo">
+          AiPro <img src={logo} alt="Logo AiPro" />
+        </Link>
+        <div className="user">
           <Select
             defaultValue="Ua"
             className="change-lang"
@@ -64,22 +78,28 @@ function Navigation() {
               },
             ]}
           />
-            <Dropdown
+          <Dropdown
             overlayClassName="dropdown-menu"
-              menu={{
-                items,
-              }}
-              placement="bottomRight"
-              trigger={["click"]}
-            >
-               <Avatar
-          className="avatar"
-          size="large"
-          src={picture|| <UserOutlined />}
-        />
-            </Dropdown>
-      </div>
-    </nav>
+            overlay={menu}
+            placement={matches ? "bottom" : "bottomRight"}
+            trigger={["click"]}
+          >
+            {matches ? (
+              <div className="burger">
+                <div className="burger-item"></div>
+                <div className="burger-item"></div>
+                <div className="burger-item"></div>
+              </div>
+            ) : (
+              <Avatar
+                className="avatar"
+                size="large"
+                src={picture || <UserOutlined />}
+              />
+            )}
+          </Dropdown>
+        </div>
+      </nav>
     </ConfigProvider>
   );
 }
